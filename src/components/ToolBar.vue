57e0378045cc40c8c3844dafed8e94b48cb242f6
Won-dy <junhfireace@gmail.com>
2022-10-16 16:36:06 +0900

    Feat: 라우터 구현

---
+++ src/components/ToolBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header">
+    <router-link to="/news">News</router-link> |
+    <router-link to="/ask">Ask</router-link> |
+    <router-link to="/jobs">Jobs</router-link>
+  </div>
+</template>
+
+<style scoped>
+/* scoped - style이 해당 컴포넌트에만 적용 */
+.header {
+  color: white;
+  background-color: #42b883;
+  display: flex;
+  padding: 8px;
+}
+.header .router-link-exact-active {
+  color: #35495e;
+}
+.header a {
+  color: white;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { router } from './router/index.js';
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,  // 축약 문법: router: router, -> router,
 }).$mount('#app')
