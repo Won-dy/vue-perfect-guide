f73acbcdcfc201a859abae89b20c76474b3304b4
Won-dy <junhfireace@gmail.com>
2022-10-16 22:55:10 +0900

    Feat: store 적용2, map 헬퍼 함수 이용

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,11 +1,42 @@
 <template>
   <div>
-    <div v-for="item in this.$store.state.ask">{{ item.title }}</div>
+    <div v-for="item in fetchedAsk">{{ item.title }}</div>
   </div>
 </template>
 
 <script>
+import {mapState, mapGetters} from 'vuex';
+
 export default {
+  computed: {
+    // #1
+    // fetchedAsk() {
+    //   return this.$store.state.ask;
+    // },
+
+    // #2
+    fetchedAsk() {
+      return this.$store.getters.fetchedAsk;
+    },
+
+    // map 헬퍼 함수 이용
+    // #3
+    // ...mapState({
+    //   fetchedAsk: state => state.ask
+    // }),
+
+    // #4
+    // ...mapGetters({
+    //   // 객체 표기법
+    //   fetchedAsk: "fetchedAsk"
+    // }),
+
+    // #5
+    // ...mapGetters([
+    //   // 배열 표기법
+    //   "fetchedAsk"
+    // ]),
+  },
   created() {
     this.$store.dispatch("FETCH_ASK");
   }
