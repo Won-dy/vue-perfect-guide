af7d6d66b60fdac33242d1c5af02cd0d14698271
Won-dy <junhfireace@gmail.com>
2022-10-16 22:21:53 +0900

    Feat: store 적용

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import {fetchNewsList} from '../api/index.js';
+import {fetchNewsList, fetchAskList, fetchJobsList} from '../api/index.js';
 
 // Vuex는 plugin 형태로 제공
 // Vuex는 상태관리도구 -> 상태:여러 컴포넌트간에 공유되는 데이터 속성
@@ -8,11 +8,19 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
-        news: []
+        news: [],
+        ask: [],
+        jobs: [],
     },
     mutations: {
         SET_NEWS(state, news) {
             state.news = news;
+        },
+        SET_ASK(state, ask) {
+            state.ask = ask;
+        },
+        SET_JOBS(state, jobs) {
+            state.jobs = jobs;
         },
     },
     actions: {
@@ -25,6 +33,29 @@ export const store = new Vuex.Store({
                 .catch(error => {
                     console.log(error);
                 });
+        },
+        FETCH_ASK({commit}) {
+            fetchAskList()
+                .then(({data}) => {
+                    commit('SET_ASK', data);
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+        // Destructuring: 구조 분해 문법
+        // FETCH_JOBS(context) {
+        FETCH_JOBS({commit}) {
+            fetchJobsList()
+                // .then(response => {
+                //     context.commit('SET_JOBS', response.data);
+                // })
+                .then(({data}) => {
+                    commit('SET_JOBS', data);
+                })
+                .catch(error => {
+                    console.log(error);
+                })
         },
     }
 })
