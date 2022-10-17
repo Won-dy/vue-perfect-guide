2a321e060f062a822b67676858e54d68b26a6b47
Won-dy <junhfireace@gmail.com>
2022-10-17 18:13:32 +0900

    Feat: 사용자 프로필(UserProfile) 컴포넌트 등록
    UserView의 하위 컴포넌트 UserProfile

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="user-container">
+    <div>
+      <i class="fas fa-user"></i>
+    </div>
+    <div class="user-description">
+      <div>
+        {{ userInfo.id }}
+        <!-- # 2 -->
+        <!-- <div>{{ info.id }}</div>-->
+      </div>
+
+      <div class="time">
+        {{ userInfo.created }}
+        <!-- # 2 -->
+        <!-- {{ info.created }}-->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // # 2. View 내용 최대한 살리기, props 사용
+  // 컴포넌트 태그 차원에서 명시적으로 표현하는 이점
+  // props: {
+  //   info: Object,
+  // },
+
+  // # 1. Profile에서 store에 접근하여 가져온다
+  // 표현할 부분에서 직접 state에 접근 - vuex 특성에 더 적합
+  computed: {
+    userInfo() {
+      return this.$store.state.user;
+    }
+  },
+}
+</script>
+
+<style scoped>
+.user-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+}
+
+.fa-user {
+  font-size: 2.5rem;
+}
+
+.user-description {
+  padding-left: 8px;
+}
+
+.time {
+  font-size: 0.7rem;
+}
+</style>
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,18 +1,29 @@
 <template>
   <div>
-    <p>name: {{ userInfo.id }}</p>
-    <p>karma: {{ userInfo.karma }}</p>
-    <p>created: {{ userInfo.created }}</p>
+    <!-- # 1 -->
+    <UserProfile></UserProfile>
+    <!-- # 2 -->
+    <!-- <UserProfile :info="userInfo"></UserProfile>-->
+
+    <!-- <p>name: {{ userInfo.id }}</p>
+        <p>karma: {{ userInfo.karma }}</p>
+        <p>created: {{ userInfo.created }}</p>-->
   </div>
 </template>
 
 <script>
+import UserProfile from '../components/UserProfile.vue'
+
 export default {
-  computed: {
-    userInfo() {
-      return this.$store.state.user;
-    }
+  components: {
+    UserProfile,
   },
+  // # 2. View 내용 최대한 살리기, props 사용
+  // computed: {
+  //   userInfo() {
+  //     return this.$store.state.user;
+  //   }
+  // },
   created() {
     const userName = this.$route.params.id;
     this.$store.dispatch('FETCH_USER', userName);
